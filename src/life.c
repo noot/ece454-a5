935dc316bfcb855a7895ef2b37b22d496db93b02
noot <elizabethjbinks@gmail.com>
2019-11-25 16:57:08 -0500

    parallelization working

--- src/life.c
+++ src/life.c
@@ -25,8 +25,13 @@
 } while(0)
 
 #define BOARD( __board, __i, __j )  (__board[(__i) + LDA*(__j)])
-#define BOARD_THREAD( __board, __i, __j, _LDA )  (__board[(__i) + (_LDA)*(__j)])
+// #define BOARD_THREAD( __board, __i, __j, _LDA )  (__board[(__i) + (_LDA)*(__j)])
 
+char* game_of_life_threads (char* outboard, 
+        char* inboard,
+        const int nrows,
+        const int ncols,
+        const int gens_max);
 
 /*****************************************************************************
  * Game of life implementation
@@ -38,17 +43,21 @@ game_of_life (char* outboard,
 	      const int ncols,
 	      const int gens_max)
 {
-  return sequential_game_of_life_threads (outboard, inboard, nrows, ncols, gens_max);
+  return game_of_life_threads (outboard, inboard, nrows, ncols, gens_max);
 }
 
 
 int done = 0;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_barrier_t barrier;
 
 typedef struct {
   int nrows;
   int ncols;
+  int starting_row;
+  int num_thread_rows;
+  int gens_max;
+  int thread_id;
   char *inboard;
   char *outboard;
 } args_t;
@@ -56,53 +65,70 @@ typedef struct {
 void * thread_func(void *vargs) {
     args_t *args = (args_t *)vargs;
 
-    int i, j;
+    int curgen, i, j;
 
-    int LDA = args->nrows;
+    //int LDA = args->nrows;
     int nrows = args->nrows;
     int ncols = args->ncols;
+    int starting_row = args->starting_row;
+    int num_thread_rows = args->num_thread_rows;
+    int gens_max = args->gens_max;
+    int thread_id = args->thread_id;
 
     char *inboard = args->inboard;
     char *outboard = args->outboard;
 
-    printf("nrows %d ncols %d\n", nrows, ncols);
+    const int LDA = nrows;
 
-    for (i = 0; i < nrows; i++)
+    //printf("[thread %d] num_thread_rows %d ncols %d starting_row %d \n", thread_id, num_thread_rows, ncols, starting_row);
+
+    for (curgen = 0; curgen < gens_max; curgen++)
     {
-        for (j = 0; j < ncols; j++)
-        {
-            const int inorth = mod (i-1, nrows);
-            const int isouth = mod (i+1, nrows);
-            const int jwest = mod (j-1, ncols);
-            const int jeast = mod (j+1, ncols);
+	   	// memset(outboard + starting_row*ncols, 0, num_thread_rows*ncols);
+	    // pthread_barrier_wait(&barrier);
 
-            const char neighbor_count = 
-                BOARD_THREAD (inboard, inorth, jwest, LDA) + 
-                BOARD_THREAD (inboard, inorth, j, LDA) + 
-                BOARD_THREAD (inboard, inorth, jeast, LDA) + 
-                BOARD_THREAD (inboard, i, jwest, LDA) +
-                BOARD_THREAD (inboard, i, jeast, LDA) + 
-                BOARD_THREAD (inboard, isouth, jwest, LDA) +
-                BOARD_THREAD (inboard, isouth, j, LDA) + 
-                BOARD_THREAD (inboard, isouth, jeast, LDA);
+        //printf("[thread %d] curgen %d\n", thread_id, curgen);
+	    for (i = starting_row; i < starting_row+num_thread_rows; i++)
+	    {
+	        for (j = 0; j < ncols; j++)
+	        {
+	            const int inorth = mod (i-1, nrows);
+	            const int isouth = mod (i+1, nrows);
+	            const int jwest = mod (j-1, ncols);
+	            const int jeast = mod (j+1, ncols);
 
-            BOARD_THREAD(outboard, i, j, LDA) = alivep (neighbor_count, BOARD_THREAD (inboard, i, j, LDA));
+	            const char neighbor_count = 
+                    BOARD (inboard, inorth, jwest) + 
+                    BOARD (inboard, inorth, j) + 
+                    BOARD (inboard, inorth, jeast) + 
+                    BOARD (inboard, i, jwest) +
+                    BOARD (inboard, i, jeast) + 
+                    BOARD (inboard, isouth, jwest) +
+                    BOARD (inboard, isouth, j) + 
+                    BOARD (inboard, isouth, jeast);
 
-        }
-    }
+                BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
+	        }
 
-    pthread_mutex_lock(&lock);
+			// if (i == starting_row+1) {
+			// 	pthread_barrier_wait(&barrier);
+			// }
+	    }
 
-    done++;
-    printf( "[thread] done is now %d. Signalling cond.\n", done );
 
-    pthread_cond_signal(&cond); 
-    pthread_mutex_unlock(&lock);
+	    // signal barrier
+	    //printf("thread %d signalling\n", thread_id);
+	    pthread_barrier_wait(&barrier);
+
+	    SWAP_BOARDS( outboard, inboard );
+
+	    //printf("barrier passed! thread %d\n", thread_id);
+	}
 
     return NULL;
 }
 
-char* sequential_game_of_life_threads (char* outboard, 
+char* game_of_life_threads (char* outboard, 
         char* inboard,
         const int nrows,
         const int ncols,
@@ -111,103 +137,64 @@ char* sequential_game_of_life_threads (char* outboard,
     /* HINT: in the parallel decomposition, LDA may not be equal to
        nrows! */
     //const int LDA = nrows;
-    int curgen, i;//, j;
+    int i;
 
     //printf("main thread nrows %d ncols %d\n", nrows, ncols, sizeof(inboard[0]));
 
-    for (i = 0; i < nrows*ncols; i++) {
-      printf("%d", inboard[i]);
-      if (i%nrows == nrows-1) printf("\n");
-    }
+    // for (i = 0; i < nrows*ncols; i++) {
+    //   printf("%d", inboard[i]);
+    //   if (i%nrows == nrows-1) printf("\n");
+    // }
 
-    int num_threads = 4;
-    pthread_t thread_ids[num_threads];
+    int num_threads = 8;
+    pthread_t thread_ids[num_threads-1];
+
+    if(pthread_barrier_init(&barrier, NULL, num_threads) != 0) {
+    	printf("could not init barrier :(");
+    	return NULL;
+    }
 
     // setup args for each thread
     args_t *args = (args_t *)malloc(num_threads*sizeof(args_t *));
 
-    int thread_nrows = nrows*2/num_threads;
-    int thread_ncols = ncols*2/num_threads;
+    for (i = 0; i < num_threads-1; i++) {
+		args[i].nrows = nrows;
+		args[i].ncols = ncols;
+		args[i].starting_row = i*nrows/num_threads;
+		args[i].num_thread_rows = nrows/num_threads;
+		args[i].gens_max = gens_max;
+		args[i].thread_id = i;
 
-    for (curgen = 0; curgen < gens_max; curgen++)
-    {
-        printf("curgen %d\n", curgen);
-        done = 0;
+		args[i].inboard = inboard;
+		args[i].outboard = outboard;
 
-        for (i = 0; i < num_threads; i++) {
-          args[i].nrows = thread_nrows;
-          args[i].ncols = thread_ncols;
+		//printf("thread id %d: %lu\n", i, thread_ids[i]);
+	    int error = pthread_create(&thread_ids[i], NULL, thread_func, (void *)&args[i]);
+		if (error != 0) {
+			printf("failed to create thread %d\n", i);
+			return NULL;
+		}
+    }
 
-          // shift inboard and outboard by nrows*i
-          args[i].inboard = (char *)malloc(sizeof(char)*thread_nrows*thread_ncols);
-          args[i].outboard = (char *)malloc(sizeof(char)*thread_nrows*thread_ncols);
+    int this_thread_id = num_threads-1;
+	args[this_thread_id].nrows = nrows;
+	args[this_thread_id].ncols = ncols;
+	args[this_thread_id].starting_row = this_thread_id*nrows/num_threads;
+	args[this_thread_id].num_thread_rows = nrows/num_threads;
+	args[this_thread_id].gens_max = gens_max;
+	args[this_thread_id].thread_id = i;
 
-          char * thread_inboard = inboard + i*thread_nrows*thread_ncols;
-          memmove(args[i].inboard, thread_inboard, thread_nrows*thread_ncols);
+	args[this_thread_id].inboard = inboard;
+	args[this_thread_id].outboard = outboard;
+    thread_func(&args[this_thread_id]);
 
-          char * thread_outboard = outboard + i*thread_nrows*thread_ncols;
-          memmove(args[i].outboard, thread_outboard, thread_nrows*thread_ncols);
-          //args[i].ncols = ncols/num_threads;
-
-          int error = pthread_create(&thread_ids[i], NULL, thread_func, (void *)&args[i]);
-          if (error != 0) {
-            printf("failed to create thread %d\n", i);
-          }
-        }
-
-        pthread_mutex_lock(&lock);
-
-        while(done < num_threads) {
-            printf( "[thread main] done is %d which is < %d so waiting on cond\n", done, num_threads );
-            
-            pthread_cond_wait(&cond, &lock); 
-            
-            //puts( "[thread main] wake - cond was signalled." ); 
-        }
-
-        pthread_mutex_unlock(&lock);
-
-        /* HINT: you'll be parallelizing these loop(s) by doing a
-           geometric decomposition of the output */
-        // for (i = 0; i < nrows; i++)
-        // {
-        //     for (j = 0; j < ncols; j++)
-        //     {
-        //         const int inorth = mod (i-1, nrows);
-        //         const int isouth = mod (i+1, nrows);
-        //         const int jwest = mod (j-1, ncols);
-        //         const int jeast = mod (j+1, ncols);
-
-        //         const char neighbor_count = 
-        //             BOARD (inboard, inorth, jwest) + 
-        //             BOARD (inboard, inorth, j) + 
-        //             BOARD (inboard, inorth, jeast) + 
-        //             BOARD (inboard, i, jwest) +
-        //             BOARD (inboard, i, jeast) + 
-        //             BOARD (inboard, isouth, jwest) +
-        //             BOARD (inboard, isouth, j) + 
-        //             BOARD (inboard, isouth, jeast);
-
-        //         BOARD(outboard, i, j) = alivep (neighbor_count, BOARD (inboard, i, j));
-
-        //     }
-        // }
-        
-        SWAP_BOARDS( outboard, inboard );
-
-        for (i = 0; i < num_threads; i++) {
-            memmove(outboard + thread_nrows*thread_ncols*i, args[i].outboard, thread_nrows*thread_ncols);
-
-           free(args[i].inboard);
-           free(args[i].outboard);
-        }
+    for(i = 0; i < num_threads-1; i++) {
+	   	//printf("thread id %d: %lu\n", i, thread_ids[i]);
+	    //pthread_join(thread_ids[i], NULL);
     }
 
     free(args);
-
-    for(i = 0; i < num_threads; i++) {
-      pthread_join(thread_ids[i], NULL);
-    }
+  	pthread_barrier_destroy(&barrier);
 
     /* 
      * We return the output board, so that we know which one contains
@@ -215,7 +202,7 @@ char* sequential_game_of_life_threads (char* outboard,
      * Just be careful when you free() the two boards, so that you don't
      * free the same one twice!!! 
      */
-    return inboard;
+    return gens_max % 2 == 1 ? outboard : inboard;
 }
 
 
